8637897c501ba6af9c9dfd8b3ffecc130755d909
Dmytro Parkhomenko <dim.parkhom2007@gmail.com>
2023-11-15 14:30:04 +0100

    update: static infos in footer

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
     <footer className="footer mt-5">
       <div className="content has-text-centered">
         <p>
-            <strong>Next.js Store</strong> by <a className="text-blue-500" href="#linkedin">Dmytro Parkhomenko</a>. The source code of this project stored on GitHub by 
-            <a className="text-blue-500" href="#source"> link</a>. The website content
+            <strong>Next.js Store</strong> by <a className="text-blue-500" href="https://www.linkedin.com/in/dmytro-parkhomenko-a874201ba/">Dmytro Parkhomenko</a>. The source code of this project stored on GitHub by 
+            <a className="text-blue-500" href="https://github.com/dmytroparkhomenko/next-store/"> link</a>. The website content
             is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
         </p>
       </div>
